17530bef004c76d1ca1c079ece0b2dec427297aa
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-04-20 12:58:30 +0530

    by malik

---
+++ backend/adminroute/adminRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+const protect = require("../middleware/protect");
+const admin = require("../middleware/adminMiddleware");
+
+const {
+  registerAdmin,
+  loginAdmin,
+  getAdminProfile,
+  logoutAdmin,
+} = require("../admincontroller/adminAuthController");
+
+
+// 🔓 Public
+router.post("/login", loginAdmin);
+
+// ⚠️ Optional (you can remove in production)
+router.post("/register", registerAdmin);
+
+
+// 🔐 Admin Protected
+router.get("/profile", protect, admin, getAdminProfile);
+router.post("/logout", protect, admin, logoutAdmin);
+
+
+module.exports = router;
