f6527f2084e5b185d01d180d7fdf08846b53775b
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-04-21 11:53:39 +0530

    store.js

--- Client/src/reducer/store.js
+++ Client/src/reducer/store.js
@@ -2,12 +2,12 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../reducer/slice/authSlice"
-import productReducer from "../reducer/slice/productSlice";
+
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    product: productReducer,
+    
   },
 });
