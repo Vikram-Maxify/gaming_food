1fe9a4ae87a74728417cb63c076ece9f1e7895af
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-04-30 15:13:24 +0530

    chef

--- chef_app/src/pages/Layout.jsx
+++ chef_app/src/pages/Layout.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
+import { Outlet } from "react-router-dom";
 
-const Layout = () => {
+const ChefLayout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="flex">
 
-export default Layout
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Right Side */}
+      <div className="flex-1 flex flex-col">
+
+        {/* Topbar */}
+        <Topbar />
+
+        {/* Page Content */}
+        <div className="p-6 bg-[#0F0F0F] min-h-screen">
+          <Outlet />
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default ChefLayout;
